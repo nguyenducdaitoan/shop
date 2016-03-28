fc3c78e06edee16065d3f698c084417d409696af
nguyenducdaitoan <nguyenducdaitoan@gmail.com>
2016-03-28 14:50:59 +0000

    First commit

--- lw_application/config/database.php
+++ lw_application/config/database.php
@@ -90,10 +90,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'store',
+	'hostname' => 'sql6.freesqldatabase.com',
+	'username' => 'sql6112640',
+	'password' => 'lGlBywgCe3',
+	'database' => 'sql6112640',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => TRUE,
